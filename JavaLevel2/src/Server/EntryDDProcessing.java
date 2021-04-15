214bf66b6df661636bb79f608d9d6f854291dae0
AlexBotya <alex_balg@bk.ru>
2021-04-15 23:10:44 +0300

    Java 3/ Lesson2 DB Entries add

--- JavaLevel2/src/Server/EntryDDProcessing.java
+++ JavaLevel2/src/Server/EntryDDProcessing.java
@@ -1,50 +1,23 @@
 package Server;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class EntryDDProcessing {
-    public List<Entry> findAll() {
-        Connection connection = DBConnection.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM CHATUSERS");
-
-            List<Entry> entries = new ArrayList<>();
-            while (resultSet.next()) {
-                entries.add(
-                        new Entry(
-                                resultSet.getString("name"),
-                                resultSet.getString("login"),
-                                resultSet.getString("password")
-                        )
-                );
-            }
-
-            return entries;
-
-        } catch (SQLException throwables) {
-            throw new RuntimeException(throwables);
-        } finally {
-            close(connection);
-        }
-    }
 
     public Optional<Entry> findByLoginAndPassword(String login, String password) {
         Connection connection = DBConnection.getConnection();
-
-
         try {
-
             try {
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM CHATUSERS WHERE LOGIN = '" + login +
-                        "' AND PASSWORD = '" + password + "'");
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM users WHERE login = ? AND password = ?"
+                );
+                preparedStatement.setString(1, login);
+                preparedStatement.setString(2, password);
+                ResultSet resultSet = preparedStatement.executeQuery();
                 if (resultSet.next()) {
                     return Optional.of(
                             new Entry(
@@ -53,9 +26,8 @@ public class EntryDDProcessing {
                                     resultSet.getString("password")
                             )
                     );
-                }
+                }return Optional.empty();
 
-                return Optional.empty();
 
             } catch (SQLException throwables) {
                 throw new RuntimeException(throwables);
@@ -63,7 +35,6 @@ public class EntryDDProcessing {
         } finally {
             close(connection);
         }
-
     }
 
     private void close(Connection connection) {
